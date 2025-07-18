9bd3c624b9edcd1c8d51c8891afb32a6a265bcde
Ethan <ej.barr00@gmail.com>
2025-07-18 17:56:20 -0400

    pagination

--- api/views.py
+++ api/views.py
@@ -14,6 +14,7 @@ from rest_framework.views import APIView
 from rest_framework import generics, filters
 from api.filters import ProductFilter, InStockFilterBackend
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination
 
 
 class ProductListCreateAPIView(generics.ListCreateAPIView):
@@ -28,6 +29,12 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
     ]
     search_fields = ["=name", "description"]
     ordering_fields = ["name", "price", "stock"]
+    pagination_class = LimitOffsetPagination
+    # pagination_class = PageNumberPagination
+    # pagination_class.page_size = 2
+    # pagination_class.page_query_param = "pagenum"
+    # pagination_class.page_size_query_param = "size"
+    # pagination_class.max_page_size = 6
 
     def get_permissions(self):
         self.permission_classes = [AllowAny]
